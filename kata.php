d237dc0f2baab680d02cebccb5880cec953006d4
Edvenan <e_vendrell@yahoo.com>
2023-07-28 10:29:23 +0200

    Optimized code - added Output msg 'your password is:'

--- kata.php
+++ kata.php
@@ -4,7 +4,8 @@ $elements = array_merge(range('A', 'Z') , range('a', 'z') , range(0,9) , ['!','@
 
 $size = intval(readline('Enter password size: '));
 
-if ($size >= 1) {
+if ($size) {
+    echo "Your password is: ";
     for ($i=0; $i < $size; $i++) { 
         echo $elements[array_rand( $elements )];
     }

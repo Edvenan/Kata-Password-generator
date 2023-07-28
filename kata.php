434b8e454a43602d9f1b146cf5528706326ac566
Edvenan <e_vendrell@yahoo.com>
2023-07-28 10:17:02 +0200

    Optimized code and bug fixed when  > elements size

--- kata.php
+++ kata.php
@@ -1,15 +1,12 @@
 <?php 
   
-$size = intval(readline('Enter password size: '));
-
 $elements = array_merge(range('A', 'Z') , range('a', 'z') , range(0,9) , ['!','@','#','$','%','^','&','*','(',')','{','}','[',']','=','<','>']);
 
-if ($size == 1) {
-    echo $elements[array_rand( $elements, $size)];
-}
-elseif ($size > 1) {
-    foreach(array_rand( $elements, $size) as $element) {
-        echo $elements[$element];
+$size = intval(readline('Enter password size: '));
+
+if ($size >= 1) {
+    for ($i=0; $i < $size; $i++) { 
+        echo $elements[array_rand( $elements )];
     }
 }
 ?>
